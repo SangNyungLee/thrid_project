648aef17e72abc656939b7c3883c31c9847c90cb
SangNyungLee <lsy6747@gmail.com>
2023-10-18 16:13:22 +0900

    수정

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get("/", function (req, res) {
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
-app.listen(8000, function () {
-  console.log(`http://localhost:8000`);
+app.listen(3000, function () {
+  console.log(`http://localhost:3000`);
 });
